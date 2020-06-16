49f22a250ce9395be3ca68da08b3e2bdd794b7ab
Michael Milroy <mdmilroy@gmail.com>
2020-06-15 22:02:42 -0400

    scaffolding for transactions

---
+++ GeneralStoreDesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using GeneralStoreDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GeneralStoreDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // On startup, load ShellViewModel as the base view, which will launch the View; similar to Startup-URI.
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
